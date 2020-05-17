513ddf9e3e454def4fdc82efa993462b78e7d9ca
brenda.nguy <brenda-nguy@hotmail.com>
2020-05-17 16:36:19 -0600

    Customized Modal Greeting

--- js/myJavaScript.js
+++ js/myJavaScript.js
@@ -1,3 +1,28 @@
+function customizeGreeting() {
+    let hour = getTimeOfDay();
+    let message = 'Hello';
+
+    if (hour >= 5 && hour < 12) {
+        message = 'Good Morning';
+    } else if (hour >=12 && hour < 18) {
+        message = 'Good Afternoon';
+    } else if (hour >= 18) {
+        message = ('Good Evening');
+    }
+    
+    displayGreetingMessage(message);
+}
+
+function displayGreetingMessage(message) {
+    document.getElementById('message').innerHTML = message;
+}
+
+function getTimeOfDay() {
+    let now = new Date();
+    let hour = now.getHours();
+    return hour;
+}
+
 function getContactData() {
     let name = document.getElementById('name').value;
     let email = document.getElementById('email').value;
@@ -12,4 +37,5 @@ function greetUser() {
 
 $(document).ready(() => {
     $('#getUserName').modal('show');
+    customizeGreeting();
 });
